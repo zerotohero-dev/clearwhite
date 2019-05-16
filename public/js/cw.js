ff3c579a7ccf670e64f004b6edd0617d843d4f82
Volkan Özçelik <me@volkan.io>
2019-05-15 19:30:32 -0700

    before fetching the main clearwhite theme css

---
+++ public/js/cw.js
@@ -0,0 +1,8 @@
+(() => {
+  const root = document.querySelector('[data-cw-root]');
+
+  root.innerHTML = `
+    <h1>Welcome to ClearWhite</h1>
+    <p>All your learning are belong to us!</p>
+  `;
+})();
