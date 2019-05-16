d2b48276bccca115a6405d0f791ee08eab8027d0
Volkan Özçelik <me@volkan.io>
2019-05-16 10:02:06 -0700

    generated folder tree seed data.

---
+++ public/js/cw-init.js
@@ -0,0 +1,2 @@
+window._cwq = window._cwq || [];
+window._cwq.push(['cw:tree:init', {"/":[".gitignore",".prettierrc","LICENSE.md","README.md","package.json","test.js","todo.txt","yarn.lock"],"/bin":["process.js"],"/public":["index-concept.html","zero-to-hero.html"],"/public/css":["main.css","reset.css","theme.css"],"/public/data":[".keep"],"/public/js":["cw.js"]}]);
--- public/js/cw.js
+++ public/js/cw.js
@@ -1,6 +1,8 @@
 (window => {
   window._cwq = window._cwq || [];
 
+  console.log('command queue', window._cwq);
+
   const root = document.querySelector('[data-clearwhite-root]');
   const style = document.createElement('style');
 
@@ -10,42 +12,12 @@
   const WIDTH_MAIN_ASIDE = '240px';
   const GUTTER = 7;
 
-  style.innerHTML = `
-    .clw-wrapper {
-      display: grid;
-      grid-gap: ${2 * GUTTER}px;
-      grid-template-areas: 'aside main';
-      grid-template-columns: ${WIDTH_MAIN_ASIDE} 1fr;
-    }
-    
-    .clw-main {
-      max-width: ${WIDTH_MAIN_MAX};
-      background: ${COLOR_MAIN_BG};
-    }
-    
-    .clw-aside {
-      grid-area: aside;
-      padding-top: ${GUTTER}px;
-      background: ${COLOR_ASIDE_BG};
-    }
-    
-    .clw-main {
-      grid-area: main;
-      padding: ${GUTTER}px;
-    }
-  `;
+  style.innerHTML = `.clw-wrapper {display: grid;grid-gap: ${2*GUTTER}px;grid-template-areas: 'aside main';grid-template-columns: ${WIDTH_MAIN_ASIDE} 1fr;}
+.clw-main {max-width: ${WIDTH_MAIN_MAX};background: ${COLOR_MAIN_BG};}
+.clw-aside {grid-area: aside;padding-top: ${GUTTER}px;background: ${COLOR_ASIDE_BG};}
+.clw-main {grid-area: main;padding: ${GUTTER}px;}`;
 
   document.getElementsByTagName('head')[0].appendChild(style);
 
-  root.innerHTML = `
-    <div class="clw-wrapper">
-      <article class="clw-main clw-panel">
-        <h1>Welcome to ClearWhite</h1>
-        <p>All your learning are belong to us!</p>
-      </article>
-      <aside class="clw-aside clw-panel">
-        <p>File list comes here</p>
-      </aside>
-    </div>
-  `;
+  root.innerHTML = `<div class="clw-wrapper"><article class="clw-main clw-panel"></article><aside class="clw-aside clw-panel"></aside></div>`;
 })(window);
